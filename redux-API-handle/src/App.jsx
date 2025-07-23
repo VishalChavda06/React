b27eaad44f7754fb7f11cfe6467c9bcc5e421df7
vishalchavda06 <vc229067@gmail.com>
2025-07-23 20:42:44 +0530

    redux-api

---
+++ redux-API-handle/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-indigo-400 text-center p-2 font-bold underline'>Welcome To Redux-api-handeling</h1>
+    </>
+  )
+}
+
+export default App
