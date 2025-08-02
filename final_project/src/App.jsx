5d2a3d48ea4ba301bc19d1b32f581e9c209be757
VishalChavda06 <vc229067@gmail.com>
2025-08-02 18:12:28 +0530

    make-footer

--- final_project/src/App.jsx
+++ final_project/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import TopHeadline from './Components/TopHeadline'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+
 function App() {
 
   return (
     <>
    <TopHeadline />
    <Navbar />
+   <Footer />
     </>
   )
 }
