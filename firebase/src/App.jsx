91f4fd5b9ed0c8f41f60ba7f7f8f23eca1cf90fd
vishalchavda06 <vc229067@gmail.com>
2025-08-19 15:14:53 +0530

    change-firebase

--- firebase/src/App.jsx
+++ firebase/src/App.jsx
@@ -1,14 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import BookList from "./Pages/Book/BookList";
-import AddBook from "./Pages/Book/AddBook"
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<BookList />} />
-        <Route path="/add" element={<AddBook />} />
-      </Routes>
-    </Router>
+    <>
+    <h1 className="text-2xl text-red-600 font-bold text-center mt-10">This is Home Page.</h1>
+    </>
   );
 }
