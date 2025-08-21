b7baff3a1707089148e8dcef9cce24cb647bd929
VishalChavda06 <vc229067@gmail.com>
2025-08-21 11:39:26 +0530

    change

--- redux-notification/src/App.jsx
+++ redux-notification/src/App.jsx
@@ -1,16 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { useDispatch } from 'react-redux';
-import { showNotification, hideNotification } from './notificationSlice.jsx';
-import Notification from './Notification.jsx';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { useDispatch } from "react-redux";
+import { showNotification, hideNotification } from "./notificationSlice.jsx";
+import Notification from "./Notification.jsx";
 
 function App() {
   const dispatch = useDispatch();
 
   const handleAddToCart = () => {
-    dispatch(showNotification('Add to Cart successfully Product'));
+    dispatch(showNotification("Add to Cart successfully Product"));
     setTimeout(() => {
       dispatch(hideNotification());
     }, 2000);
@@ -19,14 +19,28 @@ function App() {
   return (
     <>
       <Notification />
-      <h1 className='text-3xl m-3 text-center mt-3'>Redux-Notification in React</h1>
+      <h1 className="text-3xl m-3 text-center mt-3">
+        Redux-Notification in React
+      </h1>
       <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden mt-8">
-        <img className="w-full h-48 object-contain" src="https://m.media-amazon.com/images/I/313KXomNU9L._SX300_SY300_QL70_FMwebp_.jpg" alt="Product" />
+        <img
+          className="w-full h-48 object-contain"
+          src="https://m.media-amazon.com/images/I/313KXomNU9L._SX300_SY300_QL70_FMwebp_.jpg"
+          alt="Product"
+        />
         <div className="p-6">
           <h2 className="text-xl font-semibold mb-2">Sample Product</h2>
-          <p className="text-gray-700 mb-2">This is a sample product description.</p>
+          <p className="text-gray-700 mb-2">
+            This is a sample product description.
+          </p>
           <div className="flex items-center justify-between mb-4">
             <span className="text-lg font-bold text-blue-600">$19.99</span>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit in
+              deleniti natus, provident ipsam asperiores, veniam autem quas,
+              omnis officiis nam esse soluta consequatur? Perferendis nisi
+              quaerat in voluptatem culpa.
+            </p>
           </div>
           <button
             onClick={handleAddToCart}
@@ -37,7 +51,7 @@ function App() {
         </div>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
